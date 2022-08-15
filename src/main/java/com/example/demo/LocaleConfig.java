f7d777e38578154014c55654b70e0b9e26a1d8f3
mtaketani113 <47836003+mtaketani113@users.noreply.github.com>
2022-08-16 01:21:18 +0900

    不要なスペースを削除

--- src/main/java/com/example/demo/LocaleConfig.java
+++ src/main/java/com/example/demo/LocaleConfig.java
@@ -13,32 +13,32 @@ import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 @Configuration
 public class LocaleConfig implements WebMvcConfigurer  {
 
-        @Override 
-        public void addInterceptors(InterceptorRegistry registry) { 
-                registry.addInterceptor(localeChangeInterceptor()); 
-        } 
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
 
-        @Bean 
-        public LocaleChangeInterceptor localeChangeInterceptor() { 
-                LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor(); 
-                localeChangeInterceptor.setParamName("lang"); 
-                return localeChangeInterceptor; 
-        } 
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+    } 
     
-        @Bean 
-        public CookieLocaleResolver localeResolver() { 
-                CookieLocaleResolver localeResolver = new CookieLocaleResolver(); 
-                localeResolver.setDefaultLocale(Locale.JAPAN);
-                localeResolver.setCookieMaxAge(604800);
-                localeResolver.setCookiePath("/");
-                return localeResolver; 
-        } 
+    @Bean
+    public CookieLocaleResolver localeResolver() {
+        CookieLocaleResolver localeResolver = new CookieLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.JAPAN);
+        localeResolver.setCookieMaxAge(604800);
+        localeResolver.setCookiePath("/");
+        return localeResolver;
+    } 
 
-        @Bean 
-        public ReloadableResourceBundleMessageSource messageSource() { 
-            ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource(); 
-                messageSource.setBasename("classpath:i18n/message"); 
-                messageSource.setDefaultEncoding("UTF-8");
-                return messageSource; 
-        }
+    @Bean
+    public ReloadableResourceBundleMessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:i18n/message");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
 }
