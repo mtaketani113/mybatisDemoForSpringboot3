4b76ffa137e70b3e0259491120d37998d8588e73
mtaketani113 <47836003+mtaketani113@users.noreply.github.com>
2022-08-29 00:06:49 +0900

    不要なアノテーションを削除

--- src/test/java/com/example/demo/service/UserServiceTest.java
+++ src/test/java/com/example/demo/service/UserServiceTest.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,7 +19,6 @@ import com.example.demo.model.User;
 import com.example.demo.properties.HttpProxyProperties;
 
 @SpringBootTest
-@ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
 
     @Mock private RestTemplate restTemplate;
