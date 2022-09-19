9b7b8c92606fdf205beb3664aef5f1b9dd4c1888
mtaketani113 <47836003+mtaketani113@users.noreply.github.com>
2022-09-20 00:28:52 +0900

    FileダウンロードのIDを暗号化

--- src/main/java/com/example/demo/service/FileService.java
+++ src/main/java/com/example/demo/service/FileService.java
@@ -1,8 +1,11 @@
 package com.example.demo.service;
 
 import com.example.demo.aop.LatencyMonitor;
+import com.example.demo.common.Crypt;
 import com.example.demo.model.File;
 import com.example.demo.repository.FileMapper;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,15 +23,28 @@ public class FileService {
   }
 
   @LatencyMonitor
-  public File downloadFile(int id) {
+  public File downloadFile(String encryptId) {
+    int id = Integer.parseInt(Crypt.decrypt(encryptId));
     return fileMapper.download(id).orElse(new File());
   }
 
   public List<File> searchFiles() {
+    List<File> fileList = fileMapper.fileList();
+    // IDを暗号化
+    fileList.forEach(s -> {
+      try {
+        String encodedResult = URLEncoder.encode(Crypt.encrypt(s.getId()), "UTF-8");
+        s.setId(encodedResult);
+      } catch (UnsupportedEncodingException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }
+    });
     return fileMapper.fileList();
   }
 
-  public void deleteFile(int id) {
+  public void deleteFile(String encryptId) {
+    int id = Integer.parseInt(Crypt.decrypt(encryptId));
     fileMapper.delete(id);
   }
 }
