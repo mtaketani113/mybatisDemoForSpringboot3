5043de5076232887369d46ecb72f9aa5dcf3671e
mtaketani113 <47836003+mtaketani113@users.noreply.github.com>
2022-09-29 11:27:10 +0900

    ControllerAdviceのテストを追加

---
+++ src/test/java/com/example/demo/controller/DemoControllerAdviceTest.java
@@ -0,0 +1,59 @@
+package com.example.demo.controller;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import com.example.demo.controller.api.CustomerApiController;
+import com.example.demo.exceptions.DemoRuntimeException;
+import com.example.demo.response.error.ErrorResponse;
+import com.example.demo.service.CustomerService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+public class DemoControllerAdviceTest {
+    @InjectMocks
+    private CustomerApiController customerApiController;
+
+    @Mock
+    private CustomerService customerService;
+
+    private MockMvc mockMvc;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @BeforeEach
+    void init() {
+
+        mockMvc = MockMvcBuilders.standaloneSetup(customerApiController)
+                .setControllerAdvice(new DemoControllerAdvice())
+                .build();
+    }
+
+    @Test
+    public void handleEmployeeNotFoundExceptionTest() throws Exception {
+
+        when(customerService.searchById(anyString())).thenThrow(new DemoRuntimeException(""));
+
+        var responseString = mockMvc.perform(get("/api/customer/1"))
+                .andExpect(status().isInternalServerError())
+                .andReturn().getResponse().getContentAsString();
+
+        var errorResponse = objectMapper.readValue(responseString, ErrorResponse.class);
+
+        assertThat(errorResponse.getErrorCode()).isEqualTo("demoError");
+        assertThat(errorResponse.getMessage()).isEqualTo("Demo error occurred.");
+
+    }
+}
