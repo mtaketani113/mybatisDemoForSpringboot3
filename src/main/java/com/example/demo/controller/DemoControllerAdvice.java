e9cb7f00662b42100ac4f8c1b4b1efc7ffec0b2d
mtaketani113 <47836003+mtaketani113@users.noreply.github.com>
2022-09-29 11:27:00 +0900

    ControllerAdviceを追加

---
+++ src/main/java/com/example/demo/controller/DemoControllerAdvice.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import com.example.demo.exceptions.DemoRuntimeException;
+import com.example.demo.response.error.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestControllerAdvice
+public class DemoControllerAdvice {
+
+  @ExceptionHandler({DemoRuntimeException.class})
+  @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+  public ErrorResponse handleEmployeeNotFoundException(DemoRuntimeException e) {
+    log.error("Error:", e.getMessage());
+    return new ErrorResponse("demoError", "Demo error occurred.");
+  }
+}
---
+++ src/main/java/com/example/demo/response/error/ErrorResponse.java
@@ -0,0 +1,15 @@
+package com.example.demo.response.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+  String errorCode;
+  String message;
+}
