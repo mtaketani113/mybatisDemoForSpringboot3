d4659f7c84e80e575f4220a5f1e755c597849ea9
mtaketani113 <47836003+mtaketani113@users.noreply.github.com>
2022-10-06 00:52:12 +0900

    JSONの際にbuilderだと動作しないため、constractorを追加

--- src/main/java/com/example/demo/model/Customer.java
+++ src/main/java/com/example/demo/model/Customer.java
@@ -1,9 +1,15 @@
 package com.example.demo.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class Customer {
 
