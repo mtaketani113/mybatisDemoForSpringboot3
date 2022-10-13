8dab6362697bd4bc25301d8b2b97e5dda0dd6f0a
mtaketani113 <47836003+mtaketani113@users.noreply.github.com>
2022-10-14 00:04:09 +0900

    FileServiceのテストを追加

---
+++ src/test/java/com/example/demo/service/FileServiceTest.java
@@ -0,0 +1,33 @@
+package com.example.demo.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.example.demo.model.File;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Sql("/init.sql")
+@Transactional
+public class FileServiceTest {
+
+  @Autowired private FileService fileService;
+
+  @Test
+  public void ファイルの追加テスト() {
+
+    fileService.uploadFile("testFileName", new byte[0]);
+    List<File> files = fileService.searchFiles();
+
+    assertEquals(1, files.size());
+    assertEquals("testFileName", files.get(0).getFileName());
+    assertEquals(0, files.get(0).getFileData().length);
+
+  }
+
+}
