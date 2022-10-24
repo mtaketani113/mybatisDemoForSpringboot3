d5fe0007c1354d78669db74c74ae543b33c2ccda
mtaketani113 <47836003+mtaketani113@users.noreply.github.com>
2022-10-25 01:21:40 +0900

    ConstructorをUserのmodelに追加

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,9 +1,13 @@
 package com.example.demo.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 public class User {
 
