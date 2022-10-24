d3cbe1e9b7255617da4bc4c294caea53778b5899
mtaketani113 <47836003+mtaketani113@users.noreply.github.com>
2022-10-25 01:22:01 +0900

    WebSecruityにConfigureを追加

--- src/main/java/com/example/demo/WebSecurityConfig.java
+++ src/main/java/com/example/demo/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.util.Arrays;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.AuthenticationException;
@@ -19,6 +20,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import static org.springframework.security.config.Customizer.withDefaults;
 
+@Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
   @Bean
